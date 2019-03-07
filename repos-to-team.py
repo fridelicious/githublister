a2e789bef41ffb17f19f348b5e1b688bafc0211a
Frida Björklund <36764260+fridabjoo@users.noreply.github.com>
2019-03-07 13:34:07 +0100

    Update repos-to-team.py

--- repos-to-team.py
+++ repos-to-team.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 
 import requests
@@ -7,12 +6,12 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--group', required=True, help="Group to add to")
-parser.add_argument('--file', required=True, help="File to read users from")
+parser.add_argument('--file', required=True, help="File to read repos from")
+parser.add_argument('--org', required=True, help="Which organization")
 
-ORG_URL = 'https://api.github.com'
+API_URL = 'https://api.github.com'
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 GITHUB_USER = os.environ.get('GITHUB_USER')
-GITHUB_ORG = os.environ.get('GITHUB_ORG')
 
 repos = []
 
@@ -22,6 +21,8 @@ def main():
 
     repos_file = args.file
     group = args.group
+    org = args.org
+
     if not os.path.exists(repos_file):
         raise Exception("Could not find file {}".format(member_file))
     with open(repos_file) as repos_file_in:
@@ -34,15 +35,16 @@ def main():
         raise ValueError('You must set GITHUB_TOKEN')
 
     print("Adding {} repos to {}".format(len(repos), group))
-    group_exists = requests.get(ORG_URL+ '/teams/{}/repos'.format(group), auth=(GITHUB_USER, GITHUB_TOKEN))
+    group_exists = requests.get(API_URL+ '/teams/{}/repos'.format(group), auth=(GITHUB_USER, GITHUB_TOKEN))
     group_exists.raise_for_status()
     headers = {'Accept': 'application/vnd.github.hellcat-preview+json'}
     
     for repo in repos:
 
-        print("Adding {} to group {} ".format(repo,org,group))
-        team_url = ORG_URL+ '/teams/{}/repos/{}/{}'.format(group, repo)
-        r = requests.put(team_url, data='{"permission": "push"}', headers=headers, auth=(GITHUB_USER, GITHUB_TOKEN)) #read/write access 
+        print("Adding {} to group {} ".format(repo,group))
+        team_url = API_URL+ '/teams/{}/repos/{}/{}'.format(group,org,repo)
+        r = requests.put(team_url, headers=headers, auth=(GITHUB_USER, GITHUB_TOKEN)) #default read access
+        #r = requests.put(team_url, data='{"permission": "push"}', headers=headers, auth=(GITHUB_USER, GITHUB_TOKEN)) #read/write access 
     if r.status_code == 422:
         pass
     else:
